ace9851f48630a90a17e2835943000cce94303cc
Michael Wilson <mwilson@electricjones.me>
2022-01-19 12:58:48 -0500

    Push a deploy

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(
 app.use(express.json());
 
 app.get('/api/v1', (req, res) => {
-	res.send('<h2>Hi There</h2>');
+	res.send('<h2>Hi There</h2><p>This is where my frontend app will go</p>');
 	console.log('yeah it ran');
 });
 
