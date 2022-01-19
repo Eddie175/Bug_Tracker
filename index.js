32e6c93f6d87f5bc212f2e4f055e7a8cd35b978e
Michael Wilson <mwilson@electricjones.me>
2022-01-19 15:41:42 -0600

    Prepare the project for deploys to heroku (#5)
    * Remove Redis
    
    * Add Procfile for heroku
    
    * Connect to mongo based on environment
    
    * We don't need redis anymore
    
    * Push a deploy
    
    * better mongo url
    
    * create an index route
    
    * Add new env vars

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const redis = require('redis');
 const cors = require('cors');
-let RedisStore = require('connect-redis')(session);
 
 const {
 	MONGO_USER,
@@ -11,21 +9,17 @@ const {
 	MONGO_IP,
 	MONGO_PORT,
 	SESSION_SECRET,
-	REDIS_PORT,
-	REDIS_URL,
+	ENVIRONMENT,
 } = require('./config/config');
-
-let redisClient = redis.createClient({
-	host: REDIS_URL,
-	port: REDIS_PORT,
-});
 
 const userRouter = require('./routes/userRoutes');
 const ticketRouter = require('./routes/ticketRoutes');
 
 const app = express();
 
-const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+const mongoURL = (ENVIRONMENT === 'development')
+	? `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+	: process.env.DB_URL;
 
 const connectWithRetry = () => {
 	mongoose
@@ -46,7 +40,6 @@ app.enable('trust proxy');
 app.use(cors({}));
 app.use(
 	session({
-		store: new RedisStore({ client: redisClient }),
 		secret: SESSION_SECRET,
 		cookie: {
 			secure: false,
@@ -60,8 +53,8 @@ app.use(
 
 app.use(express.json());
 
-app.get('/api/v1', (req, res) => {
-	res.send('<h2>Hi There</h2>');
+app.get('/', (req, res) => {
+	res.send('<h2>Hi There</h2><p>This is where my frontend app will go</p>');
 	console.log('yeah it ran');
 });
 
